bc25b99fddefc2b1a5c6d3b388c6655e2229ea32
Zou Zhaozhen <zzou@nd.edu>
2019-11-11 10:40:28 -0500

    Update R code

--- Exercise_07.R
+++ Exercise_07.R
@@ -6,18 +6,17 @@ get_odd_rows <- function(df) {
 
 # Q2
 iris <- read.csv("iris.csv", header=TRUE)
-# find number of a given species
+# write function to find number of a given species
 num_species <- function(species) {
   return(nrow(iris[iris$Species==species,]))
 }
-num_species("setosa")
-# find entries with sepal width greater than a given value
+
+# write function to find entries with sepal width greater than a given value
 sepal_width <- function(value) {
   return(iris[iris$Sepal.Width>value,])
 }
-sepal_width(3.5)
-# write data of a given species to a csv file
+
+# write function to put data of a given species to a csv file
 species_csv <- function(species) {
   write.csv(iris[iris$Species==species,], file=paste(species,"csv",sep="."))
 }
-species_csv("setosa")
